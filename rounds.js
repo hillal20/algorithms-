d82c6dabe6299f79930bdd382ee2d4c812bb617b
hilal aissani <hilalaissani@gmail.com>
2019-02-10 19:54:18 -0500

    combination

---
+++ rounds.js
@@ -0,0 +1,44 @@
+// const pro = rounds => {
+//   const result = [];
+//   let combination;
+//   let possibilities = ["r", "s", "z"];
+
+//   const helper = (str, rounds) => {
+//     if (rounds === 0) {
+//       result.push(str);
+//       return;
+//     }
+
+//     for (let i = 0; i < possibilities.length; i++) {
+//       combination = str + possibilities[i];
+//       helper(combination, rounds - 1);
+//     }
+//   };
+
+//   helper("", rounds);
+//   return result;
+// };
+
+// console.log(pro(3));
+
+const probabilities = rounds => {
+  let input = ["r", "x", "z"];
+  let result = [];
+  let combination = "";
+
+  const helper = (str, rounds) => {
+    if (rounds === 0) {
+      result.push(str);
+      return;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+      combination = str + input[i];
+      helper(combination, rounds - 1);
+    }
+  };
+
+  helper("", rounds);
+  return result;
+};
+probabilities(3);
