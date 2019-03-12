2fb2d5ea924daca502e530257acd6b9fb14ad121
hilal aissani <hilalaissani@gmail.com>
2019-03-11 20:50:12 -0400

    longest palindrom

--- longestPalindrom.js
+++ longestPalindrom.js
@@ -1,13 +1,16 @@
-const str = "billalljsk";
+const str = "bili";
 let x = "";
-
 let maxLenght = 0;
 
-var longestPalindrome = function(str) {
-  for (let i = 0; i < str.length; i++) {
-    x = str[i];
-    for (let j = i + 1; j < str.length; j++) {
-      x += str[j];
+var longestPalindrome = function(s) {
+  let x = "";
+  let maxLenght = 0;
+  let y = "nothing";
+
+  for (let i = 0; i < s.length; i++) {
+    x = s[i];
+    for (let j = i + 1; j < s.length; j++) {
+      x += s[j];
       if (
         x ===
         x
@@ -17,11 +20,15 @@ var longestPalindrome = function(str) {
       ) {
         if (maxLenght < x.length) {
           maxLenght = x.length;
+          x.length === maxLenght ? (y = x) : null;
         }
       }
     }
   }
 
-  return maxLenght;
+  return y;
 };
 longestPalindrome(str);
+
+// let b = "hilal"
+// console.log(b.split("").reverse().join('').toString())
