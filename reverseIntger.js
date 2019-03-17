1949eb692d995791c20582aa62d9c30765b89cb7
hilal aissani <hilalaissani@gmail.com>
2019-03-16 20:17:02 -0400

    reverse

--- reverseIntger.js
+++ reverseIntger.js
@@ -22,3 +22,19 @@ console.log(
     .reverse()
     .join("")
 );
+
+const fn = y => {
+  console.log(isNaN(y.split("")[0]));
+  let x = y;
+  x = x.match(/[-]?[+]?[0-9]/g);
+
+  if (x === null || x.length === 0) {
+    return 0;
+  }
+
+  if (y.split("")[0] !== "-") {
+    return y.replace("-", "").trim();
+  }
+  return x.join("");
+};
+fn("w-42");
