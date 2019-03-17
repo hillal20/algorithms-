5aaa2393173c1832ff5d2c0d184e939811db5dfd
hilal aissani <hilalaissani@gmail.com>
2019-03-17 18:44:34 -0400

    reverse

--- regex.js
+++ regex.js
@@ -1,2 +1,3 @@
 const reg1 = /[^cbak]/g; // match everything except the letters a,b,c,d and k
 const reg2 = /[a-z]/g; // match any character from a to z
+const reg2 = /[a-z]/gi; // g means global, i case insensitive ( M or m )
