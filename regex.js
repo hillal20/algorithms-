f5c8058329847457ae4746209c58d295787745d2
hilal aissani <hilalaissani@gmail.com>
2019-03-17 18:41:55 -0400

    reverse

--- regex.js
+++ regex.js
@@ -1,1 +1,2 @@
-const reg = /[^cbak]/g; // match everything except the letters a,b,c,d and k
+const reg1 = /[^cbak]/g; // match everything except the letters a,b,c,d and k
+const reg2 = /[a-z]/g; // match any character from a to z
