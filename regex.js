968ac8d90e82eaba6894c2215f63dcc4b27be314
hilal aissani <hilalaissani@gmail.com>
2019-03-18 18:54:06 -0400

    reverse

--- regex.js
+++ regex.js
@@ -1,3 +1,5 @@
 const reg1 = /[^cbak]/g; // match everything except the letters a,b,c,d and k
 const reg2 = /[a-z]/g; // match any character from a to z
-const reg2 = /[a-z]/gi; // g means global, i case insensitive ( M or m )
+const reg3 = /[a-z]/gi; // g means global, i case insensitive ( M or m )
+const reg4 = /[0-9]+{11}/g; // + means 1 or more times
+const reg5 = /[0-9]{11}/g; // {11} repeated 11 times
