540f82d3f158a1e0c2207ca1d548868a236336e2
hilal aissani <hilalaissani@gmail.com>
2019-03-18 18:57:36 -0400

    reverse

--- regex.js
+++ regex.js
@@ -1,5 +1,6 @@
 const reg1 = /[^cbak]/g; // match everything except the letters a,b,c,d and k
 const reg2 = /[a-z]/g; // match any character from a to z
 const reg3 = /[a-z]/gi; // g means global, i case insensitive ( M or m )
-const reg4 = /[0-9]+{11}/g; // + means 1 or more times
+const reg4 = /[0-9]+/g; // + means 1 or more times
 const reg5 = /[0-9]{11}/g; // {11} repeated 11 times
+const reg6 = /[a-z]{6,11}/g; // words of 6 letters till words of 11 letters
