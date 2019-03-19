d86b7e32a08a6246606e6a9ff3f01cc4667f64f6
hilal aissani <hilalaissani@gmail.com>
2019-03-19 19:07:13 -0400

    regex

--- regex.js
+++ regex.js
@@ -4,3 +4,4 @@ const reg3 = /[a-z]/gi; // g means global, i case insensitive ( M or m )
 const reg4 = /[0-9]+/g; // + means 1 or more times
 const reg5 = /[0-9]{11}/g; // {11} repeated 11 times
 const reg6 = /[a-z]{6,11}/g; // words of 6 letters till words of 11 letters
+const reg6 = /[a-z]{6,}/g; // words of 6 letters till infinity
