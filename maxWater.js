3425fb8a69707c4ea6c1967e5faff1588027024e
hilal aissani <hilalaissani@gmail.com>
2019-03-20 19:50:22 -0400

    water

--- maxWater.js
+++ maxWater.js
@@ -2,6 +2,8 @@ var maxArea = function(arr) {
   let n = arr.length;
   let lines = [];
   let ln = 0;
+  let ln2 = 0;
+  let diff = Math.pow(10);
   let maxln = 0;
   let maxlenCor;
   let wt = 0;
@@ -10,17 +12,24 @@ var maxArea = function(arr) {
   }
   console.log(lines);
 
-  lines.forEach(e => {
+  for (let i = 0; i < lines.length; i++) {
     len = Math.sqrt(
-      Math.pow(e.endPoint[0] - e.startPoint[0], 2) +
-        Math.pow(e.endPoint[1] - e.startPoint[1], 2)
+      Math.pow(lines[i].endPoint[0] - lines[i].startPoint[0], 2) +
+        Math.pow(lines[i].endPoint[1] - lines[i].startPoint[1], 2)
     );
     console.log(len);
     if (maxln < len) {
       maxln = len;
-      maxlenCor = [maxln, e];
+      maxlenCor = [maxln, lines[i]];
     }
-  });
+
+    for (let j = i + 1; j < lines.length; j++) {
+      len2 = Math.sqrt(
+        Math.pow(lines[j].endPoint[0] - lines[j].startPoint[0], 2) +
+          Math.pow(lines[j].endPoint[1] - lines[j].startPoint[1], 2)
+      );
+    }
+  }
 
   console.log(maxlenCor);
 };
