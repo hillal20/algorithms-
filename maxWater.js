988bb10be41873dd4fe6bac21af6b4ee15651ada
hilal aissani <hilalaissani@gmail.com>
2019-03-20 18:32:29 -0400

    water

--- maxWater.js
+++ maxWater.js
@@ -1,4 +1,10 @@
 var maxArea = function(arr) {
   let n = arr.length;
+  let lines = [];
+  for (let i = 0; i < n; i++) {
+    lines.push({ startPoint: [i, 0], endPoint: [i, arr[i]] });
+  }
+  console.log(lines);
+  // a(y=max, x = 0 ) b (y=0 , x )
 };
-maxArea([1, 3, 5, 7, 8, 9, 4, 3, 2]);
+maxArea([6, 3, 5, 7, 8, 9, 4, 3, 2]);
