4855be63494bed5c9ecca09af7b5afc9db5a2e40
hilal aissani <hilalaissani@gmail.com>
2019-03-24 19:00:19 -0400

    water

--- bs.js
+++ bs.js
@@ -1,21 +1,15 @@
-const array = [1, 6];
+const array = [1, 6, 3];
 const bs = (l, h, key, arr) => {
   arr.sort();
-  console.log(arr.length);
-  console.log(key);
-  console.log(l);
-  console.log(h);
 
-  const middle = Math.floor((l + h) / 2);
-
-  if (arr.length === 1) {
-    if (arr[0] === key) {
+  if (l === h) {
+    if (arr[l] === key) {
       return true;
     } else {
       return false;
     }
   } else {
-    console.log(" ===>length > 1");
+    let middle = Math.floor((l + h) / 2);
 
     if (arr[middle] === key) {
       return true;
@@ -23,7 +17,8 @@ const bs = (l, h, key, arr) => {
 
     if (arr[middle] > key) {
       bs(l, middle - 1, key, arr);
-    } else if (arr[middle] < key) {
+    }
+    if (arr[middle] < key) {
       bs(middle + 1, h, key, arr);
     }
   }
@@ -31,4 +26,3 @@ const bs = (l, h, key, arr) => {
   return false;
 };
 bs(0, array.length - 1, 3, array);
-s;
