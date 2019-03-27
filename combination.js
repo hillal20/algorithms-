9e604b98f87d64eae197c31a2f3adad3fb0ad9ca
hilal aissani <hilalaissani@gmail.com>
2019-03-27 19:25:29 -0400

    water

--- combination.js
+++ combination.js
@@ -20,3 +20,22 @@ const pro = arr => {
 };
 
 console.log(pro(arr));
+
+let string = "alfo";
+
+const combination = string => {
+  string = string.split("");
+  const result = [];
+  let combination;
+
+  const helper = (str, string) => {
+    for (let i = 0; i < string.length; i++) {
+      combination = str + string[i];
+      result.push(combination);
+      helper(combination, string.slice(i + 1));
+    }
+  };
+  helper("", string);
+  return result;
+};
+combination(string);
